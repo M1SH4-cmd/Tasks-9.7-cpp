9f58d498c1abcabb776d512b0cea876b84632c53
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-19 23:51:22 +0700

    To be reworked (Not even working)

---
+++ Task_3/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "This program will count number of words in your string. Type EXIT to exit." << endl;
+
+    //for(;true;){
+        cout << "Enter a string:" << endl;
+        string str;
+        cin >> str;
+        int count = 1;
+
+        for(int i = 0; i < str.length(); i++){
+            if(str[i] == ' ') count++;
+            cout << count;
+        }
+        cout << "This string contains " << count + 1 << " words." << endl;
+
+        //if(str == "EXIT") break;
+    //}
+}

051b2da2e209d78b9f7475a1f5c81e19516693ac
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-19 23:34:56 +0700

    To be reworked

---
+++ Task_2/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "This programm will chech if your number is float" << endl;
+    for(;true;){
+        cout << "Enter a nubmer:" << endl;
+        string num;
+        cin >> num;
+        if(num[0] != '-' || num[0] < '0' || num[0] > '9' || num[0] != '.'){
+            cout << "incorrect value, please enter a number" << endl;
+            continue;
+        }
+
+        for(int i = 0; i < num.length(); i++){
+            if(int points = 0; (num[i] == '.')){
+                points++;
+                if((points > 1)  && (num[i] < '0' || num[i] > '9')){
+                    cout << "incorrect value, please enter a float number" << endl;
+                    break;
+                }
+                continue;
+            }
+        }
+    }
+}

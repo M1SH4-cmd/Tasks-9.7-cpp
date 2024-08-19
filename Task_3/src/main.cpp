883f9a928d17469919c251291bc331796515cb61
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-20 00:14:20 +0700

    smth reworked, not working yet

--- Task_3/src/main.cpp
+++ Task_3/src/main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
+using str = std::string;
 
 int main(){
     cout << "This program will count number of words in your string. Type EXIT to exit." << endl;
 
     //for(;true;){
         cout << "Enter a string:" << endl;
-        string str;
-        cin >> str;
+        str line;
+        cin >> line;
         int count = 1;
 
-        for(int i = 0; i < str.length(); i++){
-            if(str[i] == ' ') count++;
-            cout << count;
-        }
-        cout << "This string contains " << count + 1 << " words." << endl;
+        char arr[] = line.c_str();
+
+
+
+
+
+        // for(int i = 0; i < str.length(); i++){
+        //     if(str[i] == ' ') count++;
+        //     cout << count;
+        // }
+        // cout << "This string contains " << count + 1 << " words." << endl;
 
         //if(str == "EXIT") break;
     //}

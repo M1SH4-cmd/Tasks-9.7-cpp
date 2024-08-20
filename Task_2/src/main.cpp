994f0d8f589600a4762569387232553ba9864773
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-20 23:42:38 +0700

    Smth improoved

--- Task_2/src/main.cpp
+++ Task_2/src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     cout << "This programm will chech if your number is float" << endl;
     for(;true;){
         cout << "Enter a nubmer:" << endl;
         string num;
-        cin >> num;
+        getline(cin, num);
+        if(num == "EXIT"){
+            break;
+        }
         if(num[0] != '-' || num[0] < '0' || num[0] > '9' || num[0] != '.'){
             cout << "incorrect value, please enter a number" << endl;
             continue;
@@ -18,9 +20,8 @@ int main()
                 points++;
                 if((points > 1)  && (num[i] < '0' || num[i] > '9')){
                     cout << "incorrect value, please enter a float number" << endl;
-                    break;
+                    continue;
                 }
-                continue;
             }
         }
     }

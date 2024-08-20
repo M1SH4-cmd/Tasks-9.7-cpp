82a55b2a7c24396bf7f9b6c4e063b9b1202440e9
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-20 23:35:15 +0700

    Task 3 done!

--- Task_3/src/main.cpp
+++ Task_3/src/main.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 using namespace std;
-using str = std::string;
 
-int main(){
-    cout << "This program will count number of words in your string. Type EXIT to exit." << endl;
+int main() {
+  cout << "Type a sentence" << endl;
+  string sentence;
+  getline(cin, sentence);
 
-    //for(;true;){
-        cout << "Enter a string:" << endl;
-        str line;
-        cin >> line;
-        int count = 1;
+  int words = 0;
 
-        char arr[] = line.c_str();
+  for(int i = 0; i < sentence.size(); i++)
+  {
+    if(sentence[i] == ' ')
+    {
+      words++;
+    }
 
-
-
-
-
-        // for(int i = 0; i < str.length(); i++){
-        //     if(str[i] == ' ') count++;
-        //     cout << count;
-        // }
-        // cout << "This string contains " << count + 1 << " words." << endl;
-
-        //if(str == "EXIT") break;
-    //}
+  }
+  words = words + 1;
+  cout << "Quantity of words = " << words << endl;
 }

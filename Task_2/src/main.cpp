a8f5eb2f37fe2eef58797b595b04923d9117c7db
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-21 14:49:18 +0700

    Well, almoust working

--- Task_2/src/main.cpp
+++ Task_2/src/main.cpp
@@ -3,16 +3,23 @@ using namespace std;
 using lldouble = long double;
 int main()
 {
-    cout << "This programm will chech if your number is float" << endl;
+    cout << "This programm will chech if your number is float. Type 0 to exit" << endl;
     for(;true;){
-            lldouble num;
-            cout << "Enter a number: ";
-            cin >> num;
-            if(cin.fail()){
-                cout << "Your number is not floating point" << endl;
+        lldouble num;
+        cout << "Enter a number: ";
+        cin >> num;
+
+        if(num == 0){
+            cout << "Exiting..." << endl;
+            break;
+        }
+
+        if(cin.fail()){
+            cout << "Your number is not floating point" << endl;
+            continue;
         }else{
             cout << "Your number is a floating point" << endl;
-            break;
+            continue;
         }
     }
 }

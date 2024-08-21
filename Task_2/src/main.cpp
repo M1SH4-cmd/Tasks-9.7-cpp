08591a4e9c1f0405c868173da137833c8249956d
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-21 14:55:13 +0700

    Deleted rubbish

--- Task_2/src/main.cpp
+++ Task_2/src/main.cpp
@@ -4,22 +4,14 @@ using lldouble = long double;
 int main()
 {
     cout << "This programm will chech if your number is float. Type 0 to exit" << endl;
-    for(;true;){
-        lldouble num;
-        cout << "Enter a number: ";
-        cin >> num;
+    lldouble num;
+    cout << "Enter a number: ";
+    cin >> num;
 
-        if(num == 0){
-            cout << "Exiting..." << endl;
-            break;
+    if(cin.fail()){
+        cout << "Your number is not floating point" << endl;
+          
+    }else{
+        cout << "Your number is a floating point" << endl;
         }
-
-        if(cin.fail()){
-            cout << "Your number is not floating point" << endl;
-            continue;
-        }else{
-            cout << "Your number is a floating point" << endl;
-            continue;
-        }
-    }
 }

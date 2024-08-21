822a9037ec59355bd6462b2304292638456741d2
M1SH4-cmd <6el0zu61979@mail.ru>
2024-08-21 14:42:29 +0700

    WOW!!!!!

--- Task_2/src/main.cpp
+++ Task_2/src/main.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
 using namespace std;
+using lldouble = long double;
 int main()
 {
     cout << "This programm will chech if your number is float" << endl;
     for(;true;){
-        cout << "Enter a nubmer:" << endl;
-        string num;
-        getline(cin, num);
-        if(num == "EXIT"){
+            lldouble num;
+            cout << "Enter a number: ";
+            cin >> num;
+            if(cin.fail()){
+                cout << "Your number is not floating point" << endl;
+        }else{
+            cout << "Your number is a floating point" << endl;
             break;
-        }
-        if(num[0] != '-' || num[0] < '0' || num[0] > '9' || num[0] != '.'){
-            cout << "incorrect value, please enter a number" << endl;
-            continue;
-        }
-
-        for(int i = 0; i < num.length(); i++){
-            if(int points = 0; (num[i] == '.')){
-                points++;
-                if((points > 1)  && (num[i] < '0' || num[i] > '9')){
-                    cout << "incorrect value, please enter a float number" << endl;
-                    continue;
-                }
-            }
         }
     }
 }

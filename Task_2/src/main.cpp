fc97dd51eb9a276ee118fa67aa5e2dae424b311e
OG Michael <6el0zu61979@mail.ru>
2024-09-08 03:56:28 +0700

    All done!!!!! Hoooray!!!

--- Task_2/src/main.cpp
+++ Task_2/src/main.cpp
@@ -1,17 +1,55 @@
 #include <iostream>
-using namespace std;
-using lldouble = long double;
-int main()
-{
-    cout << "This programm will chech if your number is float. Type 0 to exit" << endl;
-    lldouble num;
-    cout << "Enter a number: ";
-    cin >> num;
+#include <string>
+#include <cctype>
 
-    if(cin.fail()){
-        cout << "Your number is not floating point" << endl;
-          
-    }else{
-        cout << "Your number is a floating point" << endl;
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Enter a number: ";
+    cin >> input;
+
+    int n = input.length();
+    if (n == 0) {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    int i = 0;
+    bool hasDigit = false;
+    bool hasDot = false;
+
+    // Проверка первого символа
+    if (input[i] == '-' || isdigit(input[i]) || input[i] == '.') {
+        if (input[i] == '.') hasDot = true;
+        i++;
+    } else {
+        cout << "No" << endl;
+        return 0;
+    }
+
+    // Проверка остальных символов
+    while (i < n) {
+        if (isdigit(input[i])) {
+            hasDigit = true;
+        } else if (input[i] == '.') {
+            if (hasDot) {
+                cout << "No" << endl;
+                return 0;
+            }
+            hasDot = true;
+        } else {
+            cout << "No" << endl;
+            return 0;
         }
+        i++;
+    }
+
+    if (hasDigit) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return 0;
 }
